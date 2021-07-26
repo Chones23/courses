c75f4334ffbbf93264a4bb81c850af2dfd9f5e3c
Kenneth2228 <komsel2228@gmail.com>
2021-07-26 19:01:50 +0700

    [finalization]

--- courses/courses/doctype/human_design_new_module/human_design_new_module.js
+++ courses/courses/doctype/human_design_new_module/human_design_new_module.js
@@ -2,7 +2,74 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Human Design New Module', {
-	refresh: function(frm) {
+	template_gates: function(frm) {
+		frm.clear_table("gates")
 
+		if(frm.doc.template_gates == undefined){
+			frm.clear_table("gates")
+			frm.refresh_field("gates");
+		}
+
+		if(frm.doc.template_gates){
+			frappe.call({
+	      method:"courses.courses.doctype.human_design_new_module.human_design_new_module.gate_temp",
+	      args:{template_gates:frm.doc.template_gates},
+	      callback:function(r){
+	        if(r.message){
+	          $.each(r.message, function(i,d){
+	            var row = frm.add_child("gates");
+							row.link_gate= d.link_gate,
+							row.read= d.read
+	          });
+	        };
+	        frm.refresh_field("gates");
+	      }
+	    })
+		}
+	},
+	template_channel: function(frm) {
+		frm.clear_table("channel")
+
+		if(frm.doc.template_channel == undefined){
+			frm.clear_table("channel")
+			frm.refresh_field("channel");
+		}
+
+		if(frm.doc.template_channel){
+			frappe.call({
+	      method:"courses.courses.doctype.human_design_new_module.human_design_new_module.chanel_temp",
+	      args:{template_channel:frm.doc.template_channel},
+	      callback:function(r){
+	        if(r.message){
+	          $.each(r.message, function(i,d){
+	            var row = frm.add_child("channel");
+							row.link_channel= d.link_channel,
+							row.read= d.read
+	          });
+	        };
+	        frm.refresh_field("channel");
+	      }
+	    })
+		}
+	},
+	onload: function(frm){
+		if(frm.doc.__islocal){
+			frappe.call({
+				method: "courses.courses.doctype.human_design_new_module.human_design_new_module.get_default_gate",
+				callback: function(data) {
+					if(data.message){
+						frappe.model.set_value(frm.doctype,frm.docname, "template_gates", data.message);
+					}
+				}
+			});
+			frappe.call({
+				method: "courses.courses.doctype.human_design_new_module.human_design_new_module.get_default_channel",
+				callback: function(data) {
+					if(data.message){
+						frappe.model.set_value(frm.doctype,frm.docname, "template_channel", data.message);
+					}
+				}
+			});
+		}
 	}
 });
--- courses/courses/doctype/human_design_new_module/human_design_new_module.py
+++ courses/courses/doctype/human_design_new_module/human_design_new_module.py
@@ -8,3 +8,49 @@ from frappe.model.document import Document
 
 class HumanDesignNewModule(Document):
 	pass
+
+@frappe.whitelist()
+def get_default_gate():
+	def_pl = frappe.db.get_value("Template Gates",{"disabled":0,"default_ndk":1},"name")
+	if def_pl not in ['' or None]:
+		return def_pl
+
+@frappe.whitelist()
+def get_default_channel():
+	def_pl = frappe.db.get_value("Template Channel",{"disabled":0,"default_ndk":1},"name")
+	if def_pl not in ['' or None]:
+		return def_pl
+
+@frappe.whitelist()
+def gate_temp(template_gates):
+	row = frappe.db.sql("""Select tgi.gates,tg.text_editor_1
+					 from `tabTemplate Gate Item` tgi
+					 inner join `tabThe Gates` tg
+					 on tgi.gates=tg.name
+					 where tgi.parent = %s
+					 order by gates asc""",template_gates,as_dict=1)
+
+	si_list = []
+	for i in row :
+		si_list.append(frappe._dict({
+			"link_gate": i.gates,
+			"read":i.text_editor_1
+		}))
+	return si_list
+
+@frappe.whitelist()
+def chanel_temp(template_channel):
+	row = frappe.db.sql("""Select tci.channel, tc.text_editor_1
+					 from `tabTemplate Channel Item` tci
+					 inner join `tabThe Channel` tc
+					 on tci.channel=tc.name
+					 where tci.parent = %s
+					 order by channel asc""",template_channel,as_dict=1)
+
+	si_list = []
+	for i in row :
+		si_list.append(frappe._dict({
+			"link_channel": i.channel,
+			"read": i.text_editor_1
+		}))
+	return si_list
